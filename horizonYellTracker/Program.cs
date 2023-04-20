0d32ce1343dba1cc29cc933e0a29222d8920bb5f
Daniel <deltasierra777@gmail.com>
2023-04-20 00:47:02 -0500

    added bazaar support, @user support, and minor fixes

--- horizonYellTracker/Program.cs
+++ horizonYellTracker/Program.cs
@@ -1,11 +1,14 @@
 ﻿using horizonYellTracker;
 using Newtonsoft.Json;
+using System.Globalization;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 
 string? webhook = null;
+string? atUser = null;
 List<string> storedQueries = new List<string>();
+List<string> storedBazQueries = new List<string>();
 var cur = Directory.GetCurrentDirectory();
 var path = Path.Combine(Directory.GetCurrentDirectory(), "config.txt");
 Config config = new();
@@ -18,6 +21,8 @@ if (File.Exists(path))
         config = tempConfig;
         if (config.Webhook != null) webhook = config.Webhook;
         storedQueries = config.Queries;
+        storedBazQueries = config.BazaarQueries;
+        atUser = config.DiscordUserId;
     }
 }
 Console.WriteLine(@"
@@ -28,15 +33,18 @@ _____.___.      .__  .__    ___________                     __
  / ______|\___  >____/____/   |____|   |__|  (____  /\___  >__|_ \\___  >__|   
  \/           \/                                  \/     \/     \/    \/         _author: Senah 
 ");
-Console.WriteLine("Tracks yells and sends them to your Discord webhook if they contain your query. Updates every 30 sec. Resets matched yells every hour.");
+Console.WriteLine(@"Tracks yells and bazaar items and sends them to your Discord webhook if they contain your query.
+Yells update every 30 sec. Bazaar items show cheapest item(up to 3 items each search), and update every 5 min.
+Resets previous matches every hour.");
 Console.WriteLine();
 
 if(webhook != null)
 {
+    Console.WriteLine("Current webhook:");
     Console.WriteLine(webhook);
     Console.WriteLine("Change current webhook? y/n");
     var webhookChangeInput = Console.ReadLine();
-    if(webhookChangeInput != null && webhookChangeInput.ToLower() == "y") webhook = null;
+    if(!string.IsNullOrEmpty(webhookChangeInput) && webhookChangeInput.ToLower() == "y") webhook = null;
 }
 
 while (webhook == null)
@@ -49,7 +57,7 @@ while (webhook == null)
         webhookToCheck = Console.ReadLine();
         Console.WriteLine("Is this correct? y/n");
         var userConfirm = Console.ReadLine();
-        if(userConfirm != null && userConfirm.ToLower() == "y") unconfirmedWebhook = false;
+        if(!string.IsNullOrEmpty(userConfirm) && userConfirm.ToLower() == "y") unconfirmedWebhook = false;
     }
 
     var discordWebhookRegex = @"https://discord.com/api/webhooks/[0-9]+\S+";
@@ -64,8 +72,57 @@ while (webhook == null)
     }
 }
 
+if (atUser != null)
+{
+    Console.WriteLine("Current Discord user ID:");
+    Console.WriteLine(atUser);
+    Console.WriteLine("Change current ID? y/n");
+    var userIDInput = Console.ReadLine();
+    if (!string.IsNullOrEmpty(userIDInput) && userIDInput.ToLower() == "y") atUser = null;
+}
+
+if (string.IsNullOrEmpty(atUser))
+{
+    string? idToCheck = null;
+    bool unconfirmedId = true;
+    bool addUser = true;
+
+    if (string.IsNullOrEmpty(config.DiscordUserId))
+    {
+        Console.WriteLine("Add @user?");
+        var addUserInput = Console.ReadLine();
+        if (string.IsNullOrEmpty(addUserInput) || addUserInput == "n") addUser = false;
+    }
+
+    if (addUser)
+    {
+        Console.WriteLine("Enter Discord ID:(This can be found by typing '\\@YourDiscordUserName' into Discord or left blank)");
+        idToCheck = Console.ReadLine();
+
+        while (unconfirmedId)
+        {
+            var discordUserRegex = @"<@[0-9]+>";
+            if (string.IsNullOrEmpty(idToCheck)) break;
+            else if (Regex.Match(idToCheck, discordUserRegex).Success)
+            {
+                atUser = idToCheck;
+                unconfirmedId = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Discord ID.");
+                unconfirmedId = true;
+            }
+        }
+    }
+    else atUser = "";
+
+
+}
+
 bool moreInput = true;
 List<string> queries = new List<string>();
+List<string> bazaarQueries = new List<string>();
 
 if (storedQueries.Any())
 {
@@ -75,12 +132,21 @@ if (storedQueries.Any())
     {
         if (query != null) Console.WriteLine($"> {query}");
     }
+    if(storedBazQueries.Any())
+    {
+        Console.WriteLine("Previous bazaar queries:");
+        foreach (var query in storedBazQueries)
+        {
+            if (query != null) Console.WriteLine($"> {query}");
+        }
+    }
     Console.WriteLine("");
     Console.WriteLine("Use previous queries? y/n");
     var usePrev = Console.ReadLine();
-    if(usePrev != null && usePrev.ToLower() == "y")
+    if(!string.IsNullOrEmpty(usePrev) && usePrev.ToLower() == "y")
     {
         queries = storedQueries;
+        bazaarQueries = storedBazQueries;
         Console.WriteLine("Add more queries? y/n");
         var addQueryInput = Console.ReadLine();
         if (addQueryInput != null && addQueryInput.ToLower() == "n") moreInput = false;
@@ -90,13 +156,15 @@ if (storedQueries.Any())
 if (moreInput)
 {
     Console.WriteLine();
-    Console.WriteLine("Add your queries(regex capable), and when your finished leave blank and hit enter.");
+    Console.WriteLine("Add your queries(regex capable). Add to bazaar queries as well with the '!' prefix. When your finished leave blank and hit enter.");
     Console.WriteLine();
 }
 
+var configBazQueries = new List<string>();
+
 while (moreInput)
 {
-    Console.WriteLine("Add yell query:");
+    Console.WriteLine("Add query:");
     var input = Console.ReadLine();
     if (string.IsNullOrEmpty(input))
     {
@@ -105,28 +173,46 @@ while (moreInput)
     }
     else
     {
-        queries.Add(input);
+        if (input.StartsWith("!"))
+        {
+            var bazaarQ = input[1..];
+            bazaarQueries.Add(bazaarQ);
+            queries.Add(bazaarQ);
+        }
+        else queries.Add(input);
     }
 }
 
+
 config.Queries = queries;
+config.BazaarQueries = bazaarQueries;
 config.Webhook = webhook;
+config.DiscordUserId = atUser;
 var configJson = JsonConvert.SerializeObject(config);
 File.WriteAllText(path, configJson);
 
+
+
 HttpClient client = new HttpClient();
-client.BaseAddress = new Uri("https://api.horizonxi.com/api/v1/misc/yells");
+HttpClient dClient = new HttpClient();
+client.BaseAddress = new Uri("https://api.horizonxi.com/api/v1");
+dClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-HttpResponseMessage response;
+HttpResponseMessage yellsResponse;
+HttpResponseMessage bazaarResponse;
 HashSet<string> matches = new HashSet<string>();
+HashSet<BazaarItem> bazMatches = new HashSet<BazaarItem>(new BazaarItemComparer());
 DateTime matchListStart = DateTime.Now;
+DateTime bazListStart = DateTime.Now;
+DateTime bazLastChecked = DateTime.Now.AddMinutes(-5);
+TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
 
 while (true)
 {
-    response = await client.GetAsync(client.BaseAddress);
-    if (response.IsSuccessStatusCode)
+    yellsResponse = await client.GetAsync($"{client.BaseAddress}/misc/yells");
+        if (yellsResponse.IsSuccessStatusCode)
     {
-        var json = response.Content.ReadAsStringAsync().Result;
+        var json = yellsResponse.Content.ReadAsStringAsync().Result;
         var yellList = JsonConvert.DeserializeObject<List<Yell>>(json);
         List<string> currentMatches = new List<string>();
         if (yellList != null) currentMatches = yellList.Where(y => queries.Any(q => Regex.Match(y.Message, @$"\b{q}\b", RegexOptions.IgnoreCase).Success)).Select(yell => $">>> [{yell.Speaker}: ](<https://horizonxi.com/players/{yell.Speaker}>): {yell.Message}").ToList();
@@ -149,12 +235,10 @@ while (true)
                     formattedYell = Regex.Replace(formattedYell, regex, m => $"__**{m.Groups[1].Value}**__", RegexOptions.IgnoreCase);
                 }
                 Console.WriteLine(yell);
-                HttpClient dClient = new HttpClient();
-                dClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var payload = new
                 {
                     username = "Yell Tracker",
-                    content = formattedYell,
+                    content = $"{formattedYell} {atUser}",
                 };
                 var contentToSend = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
                 dClient.PostAsync(webhook, contentToSend).Wait();
@@ -167,7 +251,45 @@ while (true)
             Console.WriteLine($"[{DateTime.Now.ToLocalTime()}]");
             Console.WriteLine(@">>>No new matches.");
         }
+
+        if (bazaarQueries.Any())
+        {
+            bazaarResponse = await client.GetAsync($"{client.BaseAddress}/items/bazaar");
+            var bazJson = bazaarResponse.Content.ReadAsStringAsync().Result;
+            var bazList = JsonConvert.DeserializeObject<List<BazaarItem>>(bazJson);
+            var foundBaz = new List<BazaarItem>();
+            if (bazList != null && bazList.Any() && DateTime.Now >= bazLastChecked.AddMinutes(5))
+            {
+                bazListStart = DateTime.Now;
+                var formattedBazQueries = bazaarQueries.Select(i => i.Replace(" ", "_").Replace("\\s", "_"));
+                foundBaz = bazList.Where(b => formattedBazQueries.Any(i => Regex.Match(b.Name, i, RegexOptions.IgnoreCase).Success)).OrderBy(x => x.Bazaar).ToList();
+                var filterBaz = new HashSet<BazaarItem>(new BazaarItemComparer());
+                filterBaz.UnionWith(foundBaz);
+                var cheapest = filterBaz.Take(3);
+                if (DateTime.Now >= bazListStart.AddHours(1))
+                {
+                    bazMatches = new HashSet<BazaarItem>(new BazaarItemComparer());
+                    bazListStart = DateTime.Now;
+                }
+                bazMatches.UnionWith(cheapest);
+
+                Console.WriteLine("");
+                Console.WriteLine("Baazar query matches:");
+                foreach (var item in cheapest)
+                {
+                    var itemString = $">>> [{item.Charname}](<https://horizonxi.com/players/{item.Charname}>) has **{textInfo.ToTitleCase(item.Name.Replace("_", " "))}**({item.Bazaar}g) listed in Bazaar.";
+                    Console.WriteLine(itemString);
+                    var payload = new
+                    {
+                        username = "Bazaar Tracker",
+                        content = $"{itemString} {atUser}",
+                    };
+                    var contentToSend = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                    dClient.PostAsync(webhook, contentToSend).Wait();
+                }
+            }
+        }
     }
-    else throw new Exception($"API Error: {response.StatusCode.ToString()}");
+    else throw new Exception($"API Error: {yellsResponse.StatusCode}");
     Thread.Sleep(30000);
 }
